01a7a739e346f579c131e9f9e6d7ce1a58abfdf1
RumenRT <33309rt@gmail.com>
2018-12-10 20:05:49 -0500

    add load button

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -4,8 +4,15 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def speak(data)
-    message = Message.create(body: data['message'])
-    socket = { message: message.body }
+    message = Message.new(body: data['message'])
+    if message.save
+      socket = { message: message.body, type: 'message' }
+      ChatChannel.broadcast_to('chat_channel', socket)
+  end
+
+  def load
+    messages = Message.all.collect(&:body)
+    socket = { messages: messages, type: 'messages' }
     ChatChannel.broadcast_to('chat_channel', socket)
   end
 
